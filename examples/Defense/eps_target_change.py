37a2e807bb21e5ed986ade935ac9619b62bfdd90
JieRe <1528504591@qq.com>
2021-07-20 12:04:09 +0800

    eps range(0.01,5.01)

--- examples/Defense/eps_target_change.py
+++ examples/Defense/eps_target_change.py
@@ -1,29 +1,15 @@
-# -*- coding: UTF-8 -*-
-
-import matplotlib.pyplot as plt
+import graphgallery as gg
+import graphgallery.functional as gf
+import tensorflow as tf
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import graphgallery as gg
-import graphgallery.functional as gf
-
 gg.set_backend("th")
 
 
-def softmax_cross_entropy_with_logits(labels, logits, dim=-1):
-    return (-labels * F.log_softmax(logits, dim=dim)).sum(dim=dim)
-
-
-def kld_with_logits(logit_q, logit_p):
-    q = F.softmax(logit_q, dim=-1)
-    cross_entropy = softmax_cross_entropy_with_logits(logits=logit_p, labels=q)
-    entropy = softmax_cross_entropy_with_logits(logits=logit_q, labels=q)
-    return (cross_entropy - entropy).mean()
-
-
-def forward(A, X, target):
+def forward(A, X, target, w1, w2):
     A = gf.normalize_adj_tensor(A)
     h1 = F.relu(A @ X @ w1)
     h2 = A @ h1 @ w2
@@ -31,7 +17,21 @@ def forward(A, X, target):
     return out.view(1, -1)
 
 
-def defense(graph, budget=1, eps=1):
+def defense(graph, budget, eps, target):
+    """
+    Defender Model
+    @param graph: clean graph or attacked graph
+    @param budget: maximum number of modification
+    @param eps: calibration constant
+    @param target: target node
+    @return: graph after defensive perturbation
+    """
+    trainer = gg.gallery.nodeclas.GCN(device='cpu', seed=100).make_data(graph).build()
+    trainer.fit(splits.train_nodes, splits.val_nodes)
+    w1, w2 = trainer.model.parameters()
+    w1 = w1.t()
+    w2 = w2.t()
+
     A = gf.astensor(graph.A)
     X = gf.astensor(graph.x)
     A = nn.Parameter(A.to_dense())
@@ -43,15 +43,8 @@ def defense(graph, budget=1, eps=1):
 
     edges = []
     for _ in range(int(budget)):
-        out = forward(A, X, t)
-        #         norm = torch.ones_like(out) / out.size(-1)
-        #         loss = -(kld_with_logits(norm, out) + kld_with_logits(out, norm))
-
-        #         best_wrong = (out-torch.eye(7)[out.argmax()]*1000).argmax()
-        #         loss = loss_fn(out, torch.tensor([out.argmax()]))
+        out = forward(A, X, t, w1, w2)
         coeff = F.softmax(out / eps, dim=-1).view(-1)
-        #         out = out.repeat(C, 1)
-        #         loss = loss_fn(out, torch.arange(C)) * coeff
 
         loss = 0
         for c in torch.arange(C):
@@ -81,134 +74,108 @@ def defense(graph, budget=1, eps=1):
     return defense_g
 
 
+"""
+Data Preprocessing
+"""
 data = gg.datasets.NPZDataset('cora', root='~/GraphData/datasets', transform='standardize')
 graph = data.graph
 splits = data.split_nodes(random_state=15)
 
+"""
+Attacker Model
+"""
 # GCN for attacker
-trainer = gg.gallery.nodeclas.GCN(device='cpu', seed=42).make_data(graph).build()
-trainer.fit(splits.train_nodes, splits.val_nodes)
-
-w1, w2 = trainer.model.parameters()
-w1 = w1.t()
-w2 = w2.t()
-
-################### Attacker model ############################
-W = w1 @ w2
-W = gf.tensoras(W)
-attacker = gg.attack.targeted.Nettack(graph).process(W)
+trainer_a = gg.gallery.nodeclas.GCN(device='cpu', seed=42).make_data(graph).build()
+trainer_a.fit(splits.train_nodes, splits.val_nodes)
+w1_a, w2_a = trainer_a.model.parameters()
+w1_a = w1_a.t()
+w2_a = w2_a.t()
+# attacker model
+W_a = w1_a @ w2_a
+W_a = gf.tensoras(W_a)
+attacker = gg.attack.targeted.Nettack(graph).process(W_a)
 # attacker = gg.attack.targeted.FGSM(graph).process(trainer)
 
-# GCN for defender
-trainer = gg.gallery.nodeclas.GCN(device='cpu', seed=100).make_data(graph).build()
-trainer.fit(splits.train_nodes, splits.val_nodes)
-w1, w2 = trainer.model.parameters()
-w1 = w1.t()
-w2 = w2.t()
 
-# TODO: target changes
-clean_changes = []
-attacked_changes = []
-print(f"size of splits.test_nodes {np.size(splits.test_nodes)}")
+"""
+Generate attacked_g, clean_defended_g, attacked_defended_g
+"""
+# set target, budget, eps
 # target = np.random.choice(splits.test_nodes, 1)[0]
 # target = 1000
-for target in splits.test_nodes:
 
-    attacker.set_max_perturbations()
-    attacker.reset()
-    attacker.attack(target,
-                    direct_attack=True,
-                    structure_attack=True,
-                    feature_attack=False)
-    attack_g = attacker.g
-    print(f'{attacker.num_budgets} edges has been modified.')
+# eps-target-loop, write the result out to the file
+# row is target
+# col is eps
+with open("eps(0.01,5.01)_target_change.txt", "w") as f:
+    # clean_change = []
+    # attacked_change = []
+    for target in splits.test_nodes:
+        budget = 1
+        # eps = 1
+        # true label
+        target_label = graph.node_label[target]
 
-    ################### Victim model ############################
-    # Before attack
-    trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(graph).build()
-    his = trainer.fit(splits.train_nodes,
-                      splits.val_nodes,
-                      verbose=0,
-                      epochs=100)
-    clean_predict = trainer.predict(target, transform="softmax")
+        # attacked_g
+        attacker.set_max_perturbations()
+        attacker.reset()
+        attacker.attack(target,
+                        direct_attack=True,
+                        structure_attack=True,
+                        feature_attack=False)
+        attack_g = attacker.g
+        print(f'{attacker.num_budgets} edges has been modified.')
 
-    # After attack
-    trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(attack_g).build()
-    his = trainer.fit(splits.train_nodes,
-                      splits.val_nodes,
-                      verbose=0,
-                      epochs=100)
-    attacked_predict = trainer.predict(target, transform="softmax")
-    target_label = graph.node_label[target]
+        # eps is 0 will cause divide-0 error
+        for eps in list(np.arange(0.01, 5.01, 0.01)):
+            # clean_defended_g
+            clean_defended_g = defense(graph, budget, eps, target)
 
-    ################### Defense model ############################
-    budget = 1
-    # eps = 1
-    # TODO: eps_range
-    eps_range = list(np.arange(1, 5.02, 0.02))
+            # attacked_defended_g
+            attacked_defended_g = defense(attack_g, budget, eps, target)
 
-    clean_change = []
-    attacked_change = []
-    for eps in eps_range:
-        # Defense for clean
-        trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(defense(graph, budget, eps)).build()
-        his = trainer.fit(splits.train_nodes,
-                          splits.val_nodes,
-                          verbose=0,
-                          epochs=100)
-        clean_defended_predict = trainer.predict(target, transform="softmax")
+            """
+            Prediction
+            """
+            # clean graph
+            trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(graph).build()
+            his = trainer.fit(splits.train_nodes,
+                              splits.val_nodes,
+                              verbose=0,
+                              epochs=100)
+            clean_predict = trainer.predict(target, transform="softmax")
+            clean_label = np.argmax(clean_predict)
 
-        # Defense for attacked
-        trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(defense(attack_g, budget, eps)).build()
-        his = trainer.fit(splits.train_nodes,
-                          splits.val_nodes,
-                          verbose=0,
-                          epochs=100)
-        attacked_defended_predict = trainer.predict(target, transform="softmax")
+            # attacked graph
+            trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(attack_g).build()
+            his = trainer.fit(splits.train_nodes,
+                              splits.val_nodes,
+                              verbose=0,
+                              epochs=100)
+            attacked_predict = trainer.predict(target, transform="softmax")
+            attacked_label = np.argmax(attacked_predict)
 
-        ################### Results ############################
-        #     print("eps ", eps)
-        #     print("target node ", target)
-        #     print('true label', target_label)
+            # clean defended graph
+            trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(clean_defended_g).build()
+            his = trainer.fit(splits.train_nodes,
+                              splits.val_nodes,
+                              verbose=0,
+                              epochs=100)
+            clean_defended_predict = trainer.predict(target, transform="softmax")
+            clean_defended_label = np.argmax(clean_defended_predict)
 
-        #     print('clean prediction', np.round(clean_predict, 3))
-        #     print('defended clean prediction', np.round(clean_defended_predict, 3))
-        #     print('attacked prediction', np.round(attacked_predict, 3))
-        #     print('defended attacked prediction', np.round(attacked_defended_predict, 3))
+            # attacked defended graph
+            trainer = gg.gallery.nodeclas.GCN(seed=1234567).make_data(attacked_defended_g).build()
+            his = trainer.fit(splits.train_nodes,
+                              splits.val_nodes,
+                              verbose=0,
+                              epochs=100)
+            attacked_defended_predict = trainer.predict(target, transform="softmax")
+            attacked_defended_label = np.argmax(attacked_defended_predict)
 
-        #     print("clean predict label ", np.argmax(clean_predict))
-        #     print("defended clean predict label ", np.argmax(clean_defended_predict))
-        #     print("attacked predict label ", np.argmax(attacked_predict))
-        #     print("defended attacked predict label ", np.argmax(attacked_defended_predict))
+            # clean_change.append(clean_predict[clean_label]-clean_defended_predict[clean_defended_label])
+            # attacked_change.append(attacked_predict[attacked_label]-attacked_defended_predict[attacked_defended_label])
+            f.write(f"{clean_predict[clean_label] - clean_defended_predict[clean_defended_label]},"
+                    f"{attacked_predict[attacked_label] - attacked_defended_predict[attacked_defended_label]}\t")
 
-        clean_predict_label = np.argmax(clean_predict)
-        attacked_predict_label = np.argmax(attacked_predict)
-
-        # print( f"The probability change is (before-after) {clean_predict[clean_predict_label] - clean_defended_predict[
-        # clean_predict_label]}")
-        # print( f"The probability change is (before-after) {attacked_predict[attacked_predict_label] -
-        # attacked_defended_predict[attacked_predict_label]}")
-
-        clean_change.append(clean_predict[clean_predict_label] - clean_defended_predict[clean_predict_label])
-        attacked_change.append(attacked_predict[attacked_predict_label] - attacked_defended_predict[attacked_predict_label])
-        if np.argmax(clean_predict) != np.argmax(clean_defended_predict):
-            print(
-                f"!WARNING: predict label for clean graph has been changed from {np.argmax(clean_predict)} to "
-                f"{np.argmax(clean_defended_predict)}(true label is {target_label})")
-        if np.argmax(attacked_predict) != np.argmax(attacked_defended_predict):
-            print(
-                f"!WARNING: predict label for attacked graph has been changed from {np.argmax(attacked_predict)} to "
-                f"{np.argmax(attacked_defended_predict)}(true label is {target_label})")
-
-    clean_changes.append(clean_change)
-    attacked_changes.append(attacked_change)
-
-################### TODO: Plot #########################
-x = np.size(splits.test_nodes)
-plt.figure()
-plt.xlabel('target nodes')
-plt.ylabel('change after defense')
-plt.plot(x, clean_change, color='green', linewidth=1.0, linestyle='-', label='change of clean graph')
-plt.plot(x, attacked_change, color='red', linewidth=1.0, linestyle='-', label='change of attacked graph')
-plt.legend(['change of clean graph', 'change of attacked graph'])
-plt.show()
+        f.write("\n")
